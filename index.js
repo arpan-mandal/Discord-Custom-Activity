57f3f7c8933b1247a223f0985c4118657136b3cf
Galaxy-Gazer <92933926+Galaxy-Gazer@users.noreply.github.com>
2021-10-22 18:47:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const keepAlive = require("./keep_alive")
-const { Client } = require('discord.js-selfbot-v11')
 const dotenv = require('dotenv');
-const DISCORD_TOKEN = (process.env.DISCORD_TOKEN);
+const TOKEN = (process.env.TOKEN);
+const { Client } = require('discord.js-selfbot-v11')
 const client = new Client();
 
 client.on('ready', () => { 
-  client.user.setActivity("Subscribe!", {type: "STREAMING/PLAYING/WATCHING/LISTENING", url: "https://twitch.tv/your-url"}) // Put the Type as anything, like: PLAYING, STREAMING, WATCHING etc.
-   console.log(`${client.user.username} has been set!`)
+  client.user.setActivity("Bruh", {type: "LISTENING", url: "https://twitch.tv/ninja"})
+   console.log(`Successfully Logged in as ${client.user.username}`)
 })
 
 keepAlive()
-client.login(DISCORD_TOKEN);
+client.login(TOKEN);
