d0a8d546135ed23c28c2915c724721f517c2d97e
Arpan-Mandal <92933926+Arpan-Mandal@users.noreply.github.com>
2022-04-23 18:46:59 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -3,12 +3,12 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-  res.send("Bot is running!")
+  res.send("He got dat drip on his face. NAHHHHH")
 })
 
 function keepAlive() {
   server.listen(3000, () => {
-    console.log("Server is ready.")
+    console.log("Server is up and running, Make it 24/7 now!")
   })
 }
 
